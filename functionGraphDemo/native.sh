a5b8264d5749fb14dbbb08fb095b549fee11ac59
朴海淋 <piaohailin@163.com>
2021-10-21 09:35:21 +0800

    测试 加载时间

---
+++ functionGraphDemo/native.sh
@@ -0,0 +1,4 @@
+#export PATH=/Library/Java/JavaVirtualMachines/graalvm-ce-java11-21.2.0\ 2/Contents/Home/bin:$PATH
+
+
+/Library/Java/JavaVirtualMachines/graalvm-ce-java11-21.2.0\ 2/Contents/Home/bin/node --jvm --polyglot server.js
